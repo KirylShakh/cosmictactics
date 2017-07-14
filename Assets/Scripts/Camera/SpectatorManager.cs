8c4abd9cb41aae9f96670af8e743cbff1302d2fa
anon <anon@anon.com>
2017-07-14 14:25:30 +0200

    Implement initial camera controls

---
+++ Assets/Scripts/Camera/SpectatorManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorManager : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Rigidbody rb;
+	private Vector3 velocity;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed;
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	}
+}
